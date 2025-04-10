a7699a207750d9bcbaa4b356397430205fe0b079
Jared Garcia <denisjared286@gmail.com>
2025-04-09 20:39:37 -0600

    Update: modify .gitignore and firebase configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpzXwDniTAl1mjQsTDnoyg3fR9hxS5ipg",
-  authDomain: "hn-prd-hmc.firebaseapp.com",
-  projectId: "hn-prd-hmc",
-  storageBucket: "hn-prd-hmc.firebasestorage.app",
-  messagingSenderId: "422765963438",
-  appId: "1:422765963438:web:d58c4c6154f62dceb6a53f",
-  measurementId: "G-KZPC2M8CHC",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
