06c4c80e1656411e3de183c9a4a84e9782cdb1ae
Jared Garcia <denisjared286@gmail.com>
2025-04-12 17:37:27 -0600

    Feat(Blog): Add botton on each blog card to see more

--- src/components/blog/BlogCard.vue
+++ src/components/blog/BlogCard.vue
@@ -1,19 +1,29 @@
 <template>
   <RouterLink
     :to="{ name: 'blog', params: { id: blog.id } }"
-    class="bg-white rounded-2xl shadow-md overflow-hidden transition hover:shadow-xl"
+    class="bg-white rounded-2xl shadow-md overflow-hidden transition hover:shadow-xl flex flex-col justify-between"
   >
-    <img :src="blog.image" alt="" class="w-full h-48 object-cover" />
-    <div class="p-5">
-      <p class="text-sm text-gray-500 mb-2">{{ blog.date }}</p>
-      <h3
-        class="text-lg font-semibold text-green-700 hover:text-green-900 cursor-pointer"
+    <div>
+      <img :src="blog.image" alt="" class="w-full h-48 object-cover" />
+      <div class="p-5">
+        <p class="text-sm text-gray-500 mb-2">{{ blog.date }}</p>
+        <h3
+          class="text-lg font-semibold text-green-700 hover:text-green-900 cursor-pointer"
+        >
+          {{ blog.title }}
+        </h3>
+        <p class="text-gray-600 mt-2 text-sm line-clamp-3">
+          {{ blog.description }}
+        </p>
+      </div>
+    </div>
+    <div class="flex justify-end p-3">
+      <RouterLink
+        :to="{ name: 'blog', params: { id: blog.id } }"
+        class="bg-green-200 rounded-full py-2 px-3 text-primary"
       >
-        {{ blog.title }}
-      </h3>
-      <p class="text-gray-600 mt-2 text-sm line-clamp-3">
-        {{ blog.description }}
-      </p>
+        <font-awesome-icon :icon="['fas', 'arrow-right']" />
+      </RouterLink>
     </div>
   </RouterLink>
 </template>
